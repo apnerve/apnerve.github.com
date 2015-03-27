f405b3ae06c07c5c4f61b186b72b3ba7e0c35eb8
praveen <apnerve@gmail.com>
2015-03-27 18:30:59 +0530

    added transition

--- zoomo/facelift/app.js
+++ zoomo/facelift/app.js
@@ -4,22 +4,45 @@
     populate(data.cars);
   });
 
-})(jQuery);
+  var populate = function(cars) {
+    console.log(cars);
+    var details = $('#details');
 
-var populate = function(cars) {
-  console.log(cars);
-  
-  new Vue({
-    el: '#cars',
-    data: {
-      cars: cars
-    }
-  });
-
-  var $container = $('#cars');
-  $container.imagesLoaded( function() {
-    $container.masonry({
-      gutter: 24
+    var carVM = new Vue({
+      el: '#cars',
+      data: {
+        cars: cars,
+        detail: {}
+      },
+      methods: {
+        showDetails: function(car) {
+          carVM.$data.detail = car;
+          var image  = $(car.$el).find('.img-splash');
+          console.log(image.offset());
+          $('body').addClass('overlay');
+          details.css({
+            'width' : image.width(),
+            'height' : image.height(),
+            'left' : image.offset().left + 'px',
+            'top' : image.offset().top + 'px',
+            'overflow' :'hidden'
+          }).animate({
+            'width' : '900px',
+            'height' :'2000px',
+            'left' : ($(window).width() - 900)/2 + 'px',
+            'top' : 0,
+            'overflow' : 'scroll'
+          }, 400);
+        }
+      }
     });
-  });
-}
+
+    var $container = $('#cars .list');
+    $container.imagesLoaded( function() {
+      $container.masonry({
+        gutter: 24
+      });
+    });
+  }
+
+})(jQuery);
