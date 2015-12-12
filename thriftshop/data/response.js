3f12a76aaa1a0b5beff6a77c433ce23e1a467b40
praveen <apnerve@gmail.com>
2015-12-12 19:33:37 +0530

    re-adding thriftshop

---
+++ thriftshop/data/response.js
@@ -0,0 +1,186 @@
+module.exports = JSON.parse(
+{
+  "name": "STOCK CLEARANCE SALE - BRACELETS, WHATSAPP TO 9739337444 TO BUY",
+  "photos": {
+    "data": [
+      {
+        "name": "STOCK CLEARANCE SALE - BRACELETS
+Actual price - INR 850/-
+Offer Price - INR 599/- 
+one pc available",
+        "id": "473330892835718"
+      },
+      {
+        "name": "SOLD
+STOCK CLEARANCE SALE - BRACELETS
+Actual price - INR 650/-
+Offer Price - INR 399/- 
+one pc available",
+        "id": "473330906169050"
+      },
+      {
+        "name": "SOLD
+STOCK CLEARANCE SALE - BRACELETS
+Actual price - INR 950/-
+Offer Price - INR 599/- 
+one pc available",
+        "id": "473330926169048"
+      },
+      {
+        "name": "STOCK CLEARANCE SALE - BRACELETS
+Actual price - INR 650/-
+Offer Price - INR 399/- 
+one pc available",
+        "id": "473330946169046"
+      },
+      {
+        "name": "STOCK CLEARANCE SALE - BRACELETS
+Actual price - INR 950/-
+Offer Price - INR 599/- 
+one pc available only red is available",
+        "id": "473330959502378"
+      },
+      {
+        "name": "Sold
+STOCK CLEARANCE SALE - BRACELETS
+Actual price - INR 350/-
+Offer Price - INR 200/- 
+one pc available",
+        "id": "473330979502376"
+      },
+      {
+        "name": "BLOCKED
+STOCK CLEARANCE SALE - BRACELETS
+Actual price - INR 300/-
+Offer Price - INR 150/- 
+one pc available",
+        "id": "473330992835708"
+      },
+      {
+        "name": "Sold
+STOCK CLEARANCE SALE - BRACELETS
+Actual price - INR 650/-
+Offer Price - INR 399/- 
+one pc available",
+        "id": "473331012835706"
+      },
+      {
+        "name": "STOCK CLEARANCE SALE - BRACELETS
+Actual price - INR 950/-
+Offer Price - INR 599/- 
+one pc available",
+        "id": "473331026169038"
+      },
+      {
+        "name": "STOCK CLEARANCE SALE - BRACELETS
+Actual price - INR 1150/-
+Offer Price - INR 699/- 
+one pc available",
+        "id": "473331049502369"
+      },
+      {
+        "name": "Sold
+STOCK CLEARANCE SALE - BRACELETS
+Actual price - INR 1500/-
+Offer Price - INR 799/- 
+one pc available",
+        "id": "473331072835700"
+      },
+      {
+        "name": "BLOCKED
+STOCK CLEARANCE SALE - BRACELETS
+Actual price - INR 1000/-
+Offer Price - INR 699/- 
+one pc available",
+        "id": "473331092835698"
+      },
+      {
+        "name": "BLOCKED
+STOCK CLEARANCE SALE - BRACELETS
+Actual price - INR 1000/-
+Offer Price - INR 699/- 
+one pc available",
+        "id": "473331119502362"
+      },
+      {
+        "name": "STOCK CLEARANCE SALE - BRACELETS
+Actual price - INR 950/-
+Offer Price - INR 599 /- 
+one pc available",
+        "id": "473331139502360"
+      },
+      {
+        "name": "STOCK CLEARANCE SALE - BRACELETS
+Actual price - INR 450/-
+Offer Price - INR 300/- 
+one pc available",
+        "id": "473331159502358"
+      },
+      {
+        "name": "STOCK CLEARANCE SALE - BRACELETS
+Actual price - INR 450/-
+Offer Price - INR 300/- 
+one pc available",
+        "id": "473331189502355"
+      },
+      {
+        "name": "STOCK CLEARANCE SALE - BRACELETS
+Actual price - INR 850/- per pc
+Offer Price - INR 599/- 
+3 pc available",
+        "id": "473331209502353"
+      },
+      {
+        "name": "STOCK CLEARANCE SALE - BRACELETS
+Actual price - INR 850/- per pc
+Offer Price - INR 599/- 
+3 pc available",
+        "id": "473331232835684"
+      },
+      {
+        "name": "STOCK CLEARANCE SALE - BRACELETS
+Actual price - INR 340/- per pc
+Offer Price - INR 220/- 
+1 pc available",
+        "id": "473331252835682"
+      },
+      {
+        "name": "STOCK CLEARANCE SALE - BRACELETS
+Actual price - INR 340/- per pc
+Offer Price - INR 220/- 
+1 pc available",
+        "id": "473331276169013"
+      },
+      {
+        "name": "STOCK CLEARANCE SALE - BRACELETS
+Actual price - INR 300/- per pc
+Offer Price - INR 180/- 
+1 pc available",
+        "id": "473331296169011"
+      },
+      {
+        "name": "STOCK CLEARANCE SALE - BRACELETS
+Actual price - INR 300/- per pc
+Offer Price - INR 180/- 
+1 pc available",
+        "id": "473331309502343"
+      },
+      {
+        "name": "Sold
+STOCK CLEARANCE SALE - BRACELETS
+Actual price - INR 1200/- per pc
+Offer Price - INR 699/- 
+1 pc available",
+        "id": "473332706168870"
+      }
+    ],
+    "paging": {
+      "cursors": {
+        "before": "NDczMzMwODkyODM1NzE4",
+        "after": "NDczMzMyNzA2MTY4ODcw"
+      }
+    }
+  },
+  "link": "https://www.facebook.com/album.php?fbid=473329992835808&id=232766576892152&aid=1073741867",
+  "id": "473329992835808"
+});
